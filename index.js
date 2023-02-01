9cadf514394df1353436d8ce0819fd0da72a7d29
Nathianny Pereira <nathianny_pereira26@hotmail.com>
2023-02-01 15:16:59 -0300

    projetoaula

---
+++ index.js
@@ -0,0 +1,9 @@
+var nome = prompt("Olá! Qual é o seu nome?");
+
+alert("Seja bem vinda, " + nome);
+
+var h1 = document.getElementById("projeto");
+
+h1.innerText = "Seja bem vinda, " + nome;
+
+h1.style.color = "pink";
